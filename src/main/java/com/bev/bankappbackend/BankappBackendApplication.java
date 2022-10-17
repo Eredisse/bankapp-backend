5def182fe2127f9506a72ea7fd464fe923d79429
alexandru.borza <alexandru.borza@tazz.ro>
2022-10-17 21:41:18 +0300

    Add JWT authorization

--- src/main/java/com/bev/bankappbackend/BankappBackendApplication.java
+++ src/main/java/com/bev/bankappbackend/BankappBackendApplication.java
@@ -2,8 +2,10 @@ package com.bev.bankappbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class BankappBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bev/bankappbackend/security/SecurityConfig.java
+++ src/main/java/com/bev/bankappbackend/security/SecurityConfig.java
@@ -1,12 +1,23 @@
 package com.bev.bankappbackend.security;
 
+import com.bev.bankappbackend.filter.JWTTokenGeneratorFilter;
+import com.bev.bankappbackend.filter.JWTTokenValidatorFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -14,10 +25,27 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .cors().configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setExposedHeaders(Arrays.asList("Authorization"));
+                        configuration.setMaxAge(3600L);
+                        return configuration;
+                    }
+                }).and()
+                .csrf().ignoringAntMatchers("/register").csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .and()
+                .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)
+                .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
-                .antMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards","/user").authenticated()
-                .antMatchers("/notices", "/contact","/register").permitAll()
+                .antMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards","/user","/contact").authenticated()
+                .antMatchers("/notices","/register").permitAll()
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
